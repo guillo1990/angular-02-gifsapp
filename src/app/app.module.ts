22537b73c7dcdbaf986ea23d250f682968640d74
Guillem <guillemgovi@gmail.com>
2022-09-29 13:00:02 +0200

    Classe 86

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
   ],
--- src/app/gifs/serrvices/gifs.service.ts
+++ src/app/gifs/serrvices/gifs.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
   private _historial: string[] = [];
+  private apiKey: string = 'R4gdf0HfRqtAjj1he9ASnI5AAChk3MVR';
+
+  public result: any[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
 
   get historial() {
     return [...this._historial];
   }
 
   buscarGifs(query: string) {
-    this._historial.unshift(query);
+    if (!this._historial.includes(query) && query.trim().length>0) {
+      this._historial.unshift(query);
+    }
+    this._historial = this._historial.splice(0, 10);
 
-    console.log(this._historial);
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+        this.result = resp.data;
+      })
+    // console.log(this._historial);
   }
 }
