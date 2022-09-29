3b29734507f033599b45eecc850c2239a4d1ebbf
Guillem <guillemgovi@gmail.com>
2022-09-29 09:14:09 +0200

    classe 82

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../serrvices/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent implements OnInit {
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+
+    this.gifsService.buscarGifs(valor);
+
+    this.txtBuscar.nativeElement.value = '';
+  }
+
+}
